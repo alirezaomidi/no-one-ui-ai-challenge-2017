0e49c06cd055053c1baa2fe6778e04e563aa2664
Alireza Omidi <alireza530@gmail.com>
2017-05-29 19:20:39 +0430

    Fix bug in move strategy Do not move a dooz checker to create a dooz

--- move_strategy.py
+++ move_strategy.py
@@ -14,10 +14,14 @@ def move_strategy(game):
         log('Dooz opportunities found')
         log('Checking if we can dooz by moving')
         for pos in my_opps:
-            my_neis = [i for i in game.get_neighbors()[pos] if i in game.get_my_positions()]
-            if my_neis:
-                log('Doozing by moving', my_neis[0], 'to', pos)  # TODO choose the best instead of the first
-                return game.move(game.get_board().get_cell(*my_neis[0]).get_checker(), Pos(*pos))
+            for line in game.get_lines():
+                dooz_partners = [i for i in line if i in game.get_my_positions()]
+                if pos in line and len(dooz_partners) == 2:
+                    my_neis = [i for i in game.get_neighbors()[pos]
+                               if i in game.get_my_positions() and i not in dooz_partners]
+                    if my_neis:
+                        log('Doozing by moving', my_neis[0], 'to', pos)  # TODO choose the best instead of the first
+                        return game.move(game.get_board().get_cell(*my_neis[0]).get_checker(), Pos(*pos))
 
     # Check if we can prevent the enemy from doozing
     enemy_opps = game.dooz_opportunities(game.get_opp_positions())
