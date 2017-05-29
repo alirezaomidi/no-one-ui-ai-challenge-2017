78b2f60523afc2997c08dacf089a4058c503cff9
Alireza Omidi <alireza530@gmail.com>
2017-05-29 19:22:07 +0430

    Update pop strategy prevent the enemy if you can

--- pop_strategy.py
+++ pop_strategy.py
@@ -1,12 +1,18 @@
 from Pos import Pos
+from log import log
 
 
 def pop_strategy(game):
-    try:
-        p = game.get_board().get_oppcells()[0].get_checker()
-    except:
-        p = Pos(0, 0).get_checker()
+    log()
+    log('Cycle #', game.get_cycle())
+    log('Starting pop strategy...')
 
-    return game.pop(p)
-
+    # Check if by popping any of the enemy checkers, we can prevent enemy from doozing
+    log('Checking if we can prevent enemy from doozing')
+    for enemy_pos in game.get_opp_positions():
+        cur_opps = game.dooz_opportunities([i for i in game.get_opp_positions() if i != enemy_pos])
+        if not cur_opps:
+            # pop the enemy_pos
+            log('Popping', enemy_pos)
+            return game.pop(game.get_board().get_cell(*enemy_pos).get_checker())
 
